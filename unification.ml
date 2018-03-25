5b473c5238c9e11acefb21d19b478ca1817a79dd
Khayyam Saleem <khayyam.saleem@gmail.com>
2018-03-25 18:17:40 -0400

    finished unification

--- unification.ml
+++ unification.ml
@@ -12,13 +12,13 @@ let rec mgu = function
      | UOk substitution_env when a == b -> UOk(substitution_env)
      | UOk substitution_env -> extend substitution_env a (VarType(b)); UOk(substitution_env)
      | UError (ty1, ty2) -> UError (ty1, ty2))
-  | (ty, VarType a)::rest 
+  | (ty, VarType a)::rest
     when (match SetStr.find_opt a (fv_of_type ty) with |Some _ -> false | _ -> true)
-      -> 
+      ->
     (match mgu rest with
      | UOk substitution_env -> extend substitution_env a ty; UOk(substitution_env)
      | UError(ty1, ty2) -> UError(ty1, ty2))
-  | (VarType a, ty)::rest 
+  | (VarType a, ty)::rest
     when (match SetStr.find_opt a (fv_of_type ty) with |Some _ -> false | _ -> true)
     ->
     (match mgu rest with
@@ -26,7 +26,15 @@ let rec mgu = function
      | UError(ty1, ty2) -> UError(ty1, ty2))
   | (VarType a, ty)::rest | (ty, VarType a)::rest -> UError(ty, VarType a)
   | (RefType a, RefType b) ::rest -> mgu((a,b)::rest)
- (* | (FuncType(arg1, body1), FuncType(arg2, body2))::rest -> *)
-
+  | (FuncType(arg1, body1), FuncType(arg2, body2))::rest ->
+    (match mgu [(arg1,arg2)] with
+     | UOk se_a ->
+       (match mgu [(body1,body2)] with
+        | UOk se_b ->
+          (match mgu rest with
+           | UOk substitution_env -> UOk(join [se_a; se_b; substitution_env])
+           | err -> err)
+        | err -> err)
+     | err -> err)
   |(ty1, ty2)::_ -> UError(ty1, ty2)
 
