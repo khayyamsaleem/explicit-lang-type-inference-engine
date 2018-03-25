050b7f4b674d259958dfa6d9a5edf55f72749271
Khayyam Saleem <khayyam.saleem@gmail.com>
2018-03-25 01:29:00 -0400

    Added gitignore

--- _build/unification.ml
+++
@@ -1,9 +0,0 @@
-open Ast
-
-type  unif_result = UOk of Subs.subst | UError of texpr*texpr
-
-let mgu lst:(Ast.texpr*Ast.texpr) list = failwith("later")
-(*   let rec unify lst_of_pairs subs = match lst_of_pairs with
-*     | [] -> UOk(subs)
-*     | (tya,tyb)::rest -> failwith("later")
-*   in *)
--- _build/unification.mli
+++
@@ -1,5 +0,0 @@
-
-
-type  unif_result = UOk of Subs.subst | UError of Ast.texpr*Ast.texpr
-  
-(* val mgu : (Ast.texpr*Ast.texpr) list -> unif_result *)
